106e692c88efe2aa15800a75a07e6ef47197f25e
MareksKing <marcins311@inbox.lv>
2022-03-10 14:47:25 +0200

    Added: Course class with variables

---
+++ src/model/Course.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Course {
+    private int id;
+    private String title;
+    private byte creditPoints;
+    private Professor professor;
+
+    private static int idCounter = 10000;
+}
