15f856fe8077da47d8f2292bb1d53c4fc0bbd974
MareksKing <marcins311@inbox.lv>
2022-03-10 16:11:09 +0200

    Edit: Testing functions

--- src/VeaService/VeaService.java
+++ src/VeaService/VeaService.java
@@ -2,8 +2,10 @@ package VeaService;
 import java.util.Random;
 
 import model.Course;
+import model.Grade;
 import model.ProfDeg;
 import model.Professor;
+import model.Student;
 
 public class VeaService {
     public static void main(String[] args) throws Exception {
@@ -23,5 +25,25 @@ public class VeaService {
         System.out.println(java);
         System.out.println(python);
         System.out.println(matematika);
+
+        Student stud1 = new Student("Vilnis", "Lībeks");
+        Student stud2 = new Student("Emīls", "Šeflers");
+        Student stud3 = new Student("Daniels", "Fišers");
+
+        System.out.println(stud1);
+        System.out.println(stud2);
+        System.out.println(stud3);
+
+        Grade gr1 = new Grade(4, stud1, java);
+        Grade gr2 = new Grade(7, stud1, python);
+        Grade gr3 = new Grade(9, stud1, matematika);
+
+        Grade gr4 = new Grade(7, stud2, java);
+        Grade gr5 = new Grade(9, stud2, python);
+        Grade gr6 = new Grade(6, stud2, matematika);
+        
+        Grade gr7 = new Grade(8, stud3, java);
+        Grade gr8 = new Grade(8, stud3, python);
+        Grade gr9 = new Grade(7, stud3, matematika);
     }
 }
